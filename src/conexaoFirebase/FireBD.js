089134369859514fd77746d1410784f69e284cc8
Hugo Finote <34668254+finotty@users.noreply.github.com>
2025-03-13 15:25:56 -0300

    Update FireBD.js

--- src/conexaoFirebase/FireBD.js
+++ src/conexaoFirebase/FireBD.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 //seu banco
 const firebaseConfig = {
-    apiKey: "AIzaSyDZdfBHc6Ax9so-d689YhHebLR2-N-Qd28",
-    authDomain: "registropmj-f567a.firebaseapp.com",
-    projectId: "registropmj-f567a",
-    storageBucket: "registropmj-f567a.appspot.com",
-    messagingSenderId: "1006929422469",
-    appId: "1:1006929422469:web:9809b8b6c2c9d9a0f8e1ec"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "9",
+    appId: ""
 };
 
 // Initialize Firebase
